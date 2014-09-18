6f7aa46c45d24fba84444b626c80f1fdd5c5a35f
Don Denton <don@happycollision.com>
2014-09-18 03:20:13 -0500

    Remove sensitive information
    Now that the passwords have been changed on the server, old passwords
    and usernames have been removed from version control so that these
    files can live safely on GitHub.

--- corona_assets/config.php
+++ corona_assets/config.php
@@ -4,9 +4,9 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 // Database Constants
-defined('DB_SERVER') ? null : define("DB_SERVER", "192.168.10.200");
-defined('DB_USER')   ? null : define("DB_USER", "corona");
-defined('DB_PASS')   ? null : define("DB_PASS", "ADyRgjNmK8PaJ4JFVGqqoNxoYdLfp6tFau7EYFc8CWksPkvpfH");
+defined('DB_SERVER') ? null : define("DB_SERVER", "localhost"); // Usually localhost
+defined('DB_USER')   ? null : define("DB_USER", "db_username"); // Username now stored outside version control
+defined('DB_PASS')   ? null : define("DB_PASS", "password"); // Password now stored outside version control
 defined('DB_NAME')   ? null : define("DB_NAME", "corona");
 
 ?>
